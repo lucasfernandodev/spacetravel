f6cc3c73325dade933df5dd54f80b441039ee562
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-03-27 01:40:28 -0300

    First Commit 🎉

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hy</h1>
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './style/reset.css';
+import './style/style.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
