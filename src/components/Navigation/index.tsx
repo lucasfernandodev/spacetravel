482cbf02abda2ffa5271f4735efab4e66db6e32a
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-04-12 13:23:04 -0300

    feat: Add new page technology

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -6,6 +6,7 @@ import style from "./style.module.css";
 const Navigation = () => {
 
   const currentPath = useLocation()
+  const menuRef = useRef<HTMLUListElement>(null);
   const navigationRef = useRef<HTMLElement>(null);
 
   
@@ -27,20 +28,42 @@ const Navigation = () => {
       })
     }
   }, [currentPath])
+
+  function toggleMenu(e: any){
+    if(menuRef.current && navigationRef.current){
+
+      menuRef.current.classList.toggle(style.active);
+
+      const btnMenuOpen = navigationRef.current.getElementsByClassName(style.menuOpen)[0] as HTMLElement;
+      const btnMenuClose= navigationRef.current.getElementsByClassName(style.menuClose)[0] as HTMLElement;
+
+      console.log(btnMenuOpen)
+      if(menuRef.current.classList.contains(style.active)){
+        btnMenuClose.style.display = 'flex';
+        btnMenuOpen.style.display = 'none';
+        return;
+      }
+      
+      btnMenuClose.style.display = 'none';
+      btnMenuOpen.style.display = 'flex';
+    }
+  }
   
   return (
     <nav className={style.navigation} ref={navigationRef}>
     
-      <button className={style.btnNavigation}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+      <button className={style.btnNavigation} onClick={e => toggleMenu(e)}>
+      <svg xmlns="http://www.w3.org/2000/svg" className={style.menuOpen} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <line x1="4" y1="6" x2="20" y2="6"></line>
         <line x1="4" y1="12" x2="20" y2="12"></line>
         <line x1="4" y1="18" x2="20" y2="18"></line>
       </svg>
+
+      <svg xmlns="http://www.w3.org/2000/svg" className={style.menuClose} width="20" height="21"><g fill="#D0D6F9" fillRule="evenodd"><path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z"/><path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z"/></g></svg>
       </button>
 
-      <ul className="navigation">
+      <ul className="navigation" ref={menuRef}>
       <div className={style.glass}></div>
         <li>
           <Link to="/">
--- src/pages/Technology/index.tsx
+++ src/pages/Technology/index.tsx
@@ -1,15 +1,63 @@
-import Header from '../../components/Header';
-import Container from '../../components/utils/container';
-import Layout from '../../components/utils/Layout';
-import style from './style.module.css';
+import Header from "../../components/Header";
+import Container from "../../components/utils/container";
+import Layout from "../../components/utils/Layout";
+import Typography from "../../components/utils/Typography";
+import style from "./style.module.css";
+import database from '../../lib/data.json';
+import { useRef, useState } from "react";
 
 const Technology = () => {
+
+  const {technology} = database;
+  console.log(technology)
+  const indicatorsRef = useRef<HTMLElement>(null);
+  const [currentData, setCurrentData] = useState(technology[0]);
+  
+  function handleSlider(e: React.MouseEvent<HTMLButtonElement, MouseEvent>){
+    const currentElement = e.target as HTMLElement;
+    const currentIndex = currentElement.dataset.index ? parseInt(currentElement.dataset.index) : 0;
+
+    if(typeof technology[currentIndex] !== 'undefined'){
+      setCurrentData(technology[currentIndex])
+
+      if(indicatorsRef.current){
+        const buttons = indicatorsRef.current.querySelectorAll('button');
+        buttons.forEach(e => e.dataset.state = 'hide');
+
+        currentElement.dataset.state = 'active'
+      }
+    }
+
+  }
+  
   return (
-    <Layout className={style.Technology}>
-    <Header />
-    <Container el="section" className={style.container}></Container>
+    <Layout className={style.technology}>
+      <Header />
+      <Container el="section" className={style.container}>
+        <Typography mask="heading5" className={style.presentationTitle}>
+          <span>03</span>
+          SPACE LAUNCH 101
+        </Typography>
+        <div className={style.slider}>
+          <section className={style.SliderWrapper}>
+            <aside className={style.sliderIndicators} ref={indicatorsRef}>
+              <button onClick={e => handleSlider(e)}className={style.btn} data-index="0" data-state="active">1</button>
+              <button onClick={e => handleSlider(e)}className={style.btn} data-index="1">2</button>
+              <button onClick={e => handleSlider(e)}className={style.btn} data-index="2">3</button>
+            </aside>
+            <article className={style.sliderContent}>
+              <Typography className={style.subtitle} mask="navText">THE TERMINOLOGY…</Typography>
+              <Typography className={style.title} mask="heading3">{currentData.name}</Typography>
+              <Typography className={style.description} mask="Text">{currentData.description}</Typography>
+            </article>
+          </section>
+          <section className={style.sliderImages}>
+            <img src={currentData.images.portrait} alt={currentData.name} />
+          </section>
+        </div>
+      </Container>
     </Layout>
-  )
-}
+  );
+};
 
 export default Technology;
